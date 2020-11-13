c118efcf5515a303f0889bf03f163b79adc610e5
krishna5137 <inbox.krishna95@gmail.com>
2020-11-13 13:37:04 +0530

    add reviews model and form

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const CampGroundSchema = new Schema({
@@ -6,7 +7,13 @@ const CampGroundSchema = new Schema({
     price: Number,
     image: String,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampGroundSchema);
