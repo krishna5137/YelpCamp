2efa257abd518b8a159b0d530eb2af5e3fd670d9
krishna5137 <inbox.krishna95@gmail.com>
2020-11-17 15:03:20 +0530

    add user login

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,34 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
+const asyncError = require('../utils/asyncError');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', asyncError(async (req, res) => {
     try {
         const { username, email, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
         req.flash('success', 'Welcome to Yelp Camp');
         res.redirect('/campgrounds');
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
     }
+}));
 
-})
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
+});
+
 
 module.exports = router;
