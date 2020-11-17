0f6dd32d8ab0887f412c861040827ecc18a9628c
krishna5137 <inbox.krishna95@gmail.com>
2020-11-17 14:23:11 +0530

    add user registration route

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const LocalStrategy = require('passport-local');
 
 const ExpressError = require('./utils/ExpressError');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/yelpcamp', {
@@ -75,8 +76,9 @@ app.use((req, res, next) => {
 /**
  * define the campgrounds, reviews routes
  */
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
