dc4dd2d6d253ddbe4145b6e324f0addf58c2ae22
krishna5137 <inbox.krishna95@gmail.com>
2020-11-19 12:44:24 +0530

    add cloudinary config for image uplaods

---
+++ cloudinary/config.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+});
+
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,9 +4,17 @@ const asyncError = require('../utils/asyncError');
 const Campground = require('../models/campground');
 const campgrounds = require('../controllers/campgrounds');
 
+//  a node.js middleware for handling multipart/form-data, which is primarily used for uploading files
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 router.route('/')
     .get(asyncError(campgrounds.index))
-    .post(isLoggedIn, validateCampground, asyncError(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, asyncError(campgrounds.createCampground));
+    .post(upload.single('image'), (req, res) => {
+        console.log(req.body, req.file);
+        res.send('Upload successful');
+    });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
