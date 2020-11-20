b3eef7b7930643f7530bc92a3decf0522c0478c4
krishna5137 <inbox.krishna95@gmail.com>
2020-11-20 16:16:45 +0530

    pin map[F to campground location

--- public/js/showMap.js
+++ public/js/showMap.js
@@ -3,6 +3,10 @@ mapboxgl.accessToken = map_access_token;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates,  // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
